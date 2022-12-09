49ccf4933edf6a08562129ea000ef3970527c630
Howard Tang <aychtang+gh@gmail.com>
2022-12-09 22:36:50 +0000

    Add ci-build script

--- src/pages/Index/Index.test.tsx
+++ src/pages/Index/Index.test.tsx
@@ -2,23 +2,24 @@ import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./Index";
 
 const indexPage = {
-    isCounterValue: (count: number) => screen.getByText(`counter value is ${count}`),
-    clickButton: (command: "increment" | "decrement" | "reset") => {
+    render: () => render(<App />),
+    getResetButton: (): HTMLElement => screen.getByText('reset'),
+    isCounterValue: (count: number): HTMLElement => screen.getByText(`counter value is ${count}`),
+    clickButton: (command: "increment" | "decrement" | "reset"): void => {
         const buttonElement = screen.getByText(command);
         fireEvent.click(buttonElement);
     },
-    getResetButton: () => screen.getByText('reset'),
-}
+};
 
 describe("Index.test.tsx - Index Page", () => {
     describe("counting behaviour", () => {
         it("should start the count at 0", () => {
-            render(<App />);
+            indexPage.render()
             expect(indexPage.isCounterValue(0)).toBeTruthy();
         })
 
         it("should increment the count when the user clicks the increment button", () => {
-            render(<App />);
+            indexPage.render()
 
             indexPage.clickButton('increment');
             expect(indexPage.isCounterValue(1)).toBeTruthy();
@@ -28,14 +29,14 @@ describe("Index.test.tsx - Index Page", () => {
         })
 
         it("should decrement the count when the user clicks the decrement button", () => {
-            render(<App />);
+            indexPage.render()
 
             indexPage.clickButton('decrement');
             expect(indexPage.isCounterValue(-1)).toBeTruthy();
         })
 
         it("should allow user to increment and decrement the count continously", () => {
-            render(<App />);
+            indexPage.render()
 
             indexPage.clickButton('decrement');
             expect(indexPage.isCounterValue(-1)).toBeTruthy();
@@ -48,7 +49,7 @@ describe("Index.test.tsx - Index Page", () => {
         })
 
         it("should allow user to reset the counter value", () => {
-            render(<App />);
+            indexPage.render()
 
             indexPage.clickButton('decrement');
             expect(indexPage.isCounterValue(-1)).toBeTruthy();
@@ -61,7 +62,7 @@ describe("Index.test.tsx - Index Page", () => {
         })
 
         it("should disable the reset button when the count is 0", () => {
-            render(<App />);
+            indexPage.render()
 
             expect(indexPage.getResetButton()).toBeDisabled()
         })

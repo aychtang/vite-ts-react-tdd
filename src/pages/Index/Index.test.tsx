84a71b72256d0562f3008b8a6930e32b6cc12539
Howard Tang <aychtang+gh@gmail.com>
2022-12-09 16:37:38 +0000

    Add decrement button

--- src/pages/Index/Index.test.tsx
+++ src/pages/Index/Index.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./Index";
 
-const getCount = (count: number) => screen.getByText(`count is ${count}`)
+const test = { getCount: (count: number) => screen.getByText(`count is ${count}`) }
 
 describe("App", () => {
     it("should render helper text", () => {
@@ -12,19 +12,42 @@ describe("App", () => {
     describe("counting behaviour", () => {
         it("should start the count at 0", () => {
             render(<App />);
-            expect(getCount(0)).toBeInTheDocument();
+            expect(test.getCount(0)).toBeInTheDocument();
         })
 
-        it("should increment the count when the user clicks the button", () => {
+        it("should increment the count when the user clicks the increment button", () => {
             render(<App />);
-            const buttonElement = getCount(0);
+
+            const buttonElement = test.getCount(0);
             expect(buttonElement).toBeInTheDocument();
+            fireEvent.click(buttonElement);
+            expect(test.getCount(1)).toBeInTheDocument();
 
             fireEvent.click(buttonElement);
-            expect(getCount(1)).toBeInTheDocument();
+            expect(test.getCount(2)).toBeInTheDocument();
+        })
 
-            fireEvent.click(buttonElement);
-            expect(getCount(2)).toBeInTheDocument();
+        it("should decrement the count when the user clicks the decrement button", () => {
+            render(<App />);
+
+            const decrementButtonElement = screen.getByText("decrement");
+            fireEvent.click(decrementButtonElement);
+            expect(test.getCount(-1)).toBeInTheDocument();
+        })
+
+        it("should allow user to increment and decrement the count continously", () => {
+            render(<App />);
+
+            const decrementButtonElement = screen.getByText("decrement");
+            fireEvent.click(decrementButtonElement);
+            const incrementButtonElement = test.getCount(-1);
+            expect(incrementButtonElement).toBeInTheDocument();
+
+            fireEvent.click(incrementButtonElement);
+            expect(test.getCount(0)).toBeInTheDocument();
+
+            fireEvent.click(incrementButtonElement);
+            expect(test.getCount(1)).toBeInTheDocument();
         })
     })
 });

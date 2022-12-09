07aab7742e1ce4d4f0bf1e10d0fd87e1c4a8cfe0
Howard Tang <aychtang+gh@gmail.com>
2022-12-09 15:53:55 +0000

    Initial commit: Set up Jest + RTL

---
+++ src/App.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+describe("App", () => {
+    it("should render helper text", () => {
+        render(<App />);
+        expect(screen.getByText("Click on the Vite and React logos to learn more")).toBeInTheDocument();
+    });
+});

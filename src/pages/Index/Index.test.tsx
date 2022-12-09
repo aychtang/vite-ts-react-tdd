dd5dbe5b9d5e83e7c40c66f2fb49abffca28048e
Howard Tang <aychtang+gh@gmail.com>
2022-12-09 17:10:40 +0000

    Refactor POM + tests for indexPage

--- src/pages/Index/Index.test.tsx
+++ src/pages/Index/Index.test.tsx
@@ -1,50 +1,48 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./Index";
 
-const test = {
-    getCount: (count: number) => screen.getByText(`count is ${count}`),
-    getIncrementButton: () => screen.getByText(`increment`),
-    getDecrementButton: () => screen.getByText(`decrement`)
+const indexPage = {
+    isCounterValue: (count: number) => screen.getByText(`count is ${count}`),
+    clickButton: (command: "increment" | "decrement") => {
+        const buttonElement = screen.getByText(command);
+        fireEvent.click(buttonElement);
+    }
 }
 
 describe("Index.test.tsx - Index Page", () => {
     describe("counting behaviour", () => {
         it("should start the count at 0", () => {
             render(<App />);
-            expect(test.getCount(0)).toBeInTheDocument();
+            expect(indexPage.isCounterValue(0)).toBeTruthy();
         })
 
         it("should increment the count when the user clicks the increment button", () => {
             render(<App />);
 
-            const buttonElement = test.getIncrementButton();
-            expect(buttonElement).toBeInTheDocument();
+            indexPage.clickButton('increment');
+            expect(indexPage.isCounterValue(1)).toBeTruthy();
 
-            fireEvent.click(buttonElement);
-            expect(test.getCount(1)).toBeInTheDocument();
-
-            fireEvent.click(buttonElement);
-            expect(test.getCount(2)).toBeInTheDocument();
+            indexPage.clickButton('increment');
+            expect(indexPage.isCounterValue(2)).toBeTruthy();
         })
 
         it("should decrement the count when the user clicks the decrement button", () => {
             render(<App />);
 
-            fireEvent.click(test.getDecrementButton());
-            expect(test.getCount(-1)).toBeInTheDocument();
+            indexPage.clickButton('decrement');
+            expect(indexPage.isCounterValue(-1)).toBeTruthy();
         })
 
         it("should allow user to increment and decrement the count continously", () => {
             render(<App />);
 
-            fireEvent.click(test.getDecrementButton());
+            indexPage.clickButton('decrement');
 
-            const incrementButtonElement = test.getIncrementButton();
-            fireEvent.click(incrementButtonElement);
-            expect(test.getCount(0)).toBeInTheDocument();
+            indexPage.clickButton('increment');
+            expect(indexPage.isCounterValue(0)).toBeTruthy();
 
-            fireEvent.click(incrementButtonElement);
-            expect(test.getCount(1)).toBeInTheDocument();
+            indexPage.clickButton('increment');
+            expect(indexPage.isCounterValue(1)).toBeTruthy();
         })
     })
 });

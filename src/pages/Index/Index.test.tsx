465d468db96be77457ca81fb08870af81472b3e4
Howard Tang <aychtang+gh@gmail.com>
2022-12-09 16:08:24 +0000

    Structure repo to have page/<pageName> folders

--- src/pages/Index/Index.test.tsx
+++ src/pages/Index/Index.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import App from "./App";
+import App from "./Index";
 
 describe("App", () => {
     it("should render helper text", () => {

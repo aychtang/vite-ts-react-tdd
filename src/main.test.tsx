b94ee5d54d2de8f7d8844b18c4fdab9d6cc3d520
Howard Tang <aychtang+gh@gmail.com>
2022-12-10 14:51:35 +0000

    Set prettier size to 4 spaces

---
+++ src/main.test.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { MainElements } from "./main";
+
+jest.mock("react-dom", () => ({ render: jest.fn() }));
+
+describe("Application root", () => {
+    it("should render without crashing", () => {
+        const div = document.createElement("div");
+        div.id = "root";
+        document.body.appendChild(div);
+        require("./main.tsx");
+        expect(ReactDOM.render).toHaveBeenCalledWith(MainElements, div);
+    });
+});
--- src/routes.tsx
+++ src/routes.tsx
@@ -2,8 +2,8 @@ import IndexPage from "./pages/Index/Index";
 import { createBrowserRouter } from "react-router-dom";
 
 export default createBrowserRouter([
-  {
-    path: "/",
-    element: <IndexPage />,
-  },
+    {
+        path: "/",
+        element: <IndexPage />,
+    },
 ]);

7d834e03c56402e9a5da0bdb85744b614ddfb79d
Howard Tang <aychtang+gh@gmail.com>
2022-12-14 23:13:42 +0000

    Rename locationName -> location

--- src/domain/weather/service/TemperatureFactory.ts
+++ src/domain/weather/service/TemperatureFactory.ts
@@ -2,20 +2,20 @@ import { GeocodedLocation, NominatumClient } from "../data/clients/Geocoding";
 import { OpenMateoClient } from "../data/clients/Weather";
 import { Temperature } from "../data/Temperature";
 
-const geocodeLocation = async (locationName: string) =>
-	await new NominatumClient().geocodeLocation(locationName);
+const geocodeLocation = async (location: string) =>
+	await new NominatumClient().geocodeLocation(location);
 
 const getTemperatureForGeocodedLocation = async (location: GeocodedLocation) =>
 	await new OpenMateoClient().getWeatherForLatLon(location);
 
 export class TemperatureFactory {
-	public async getTemperatureAt(locationName: string): Promise<Temperature> {
-		const geocodedLocation = await geocodeLocation(locationName);
+	public async getTemperatureAt(location: string): Promise<Temperature> {
+		const geocodedLocation = await geocodeLocation(location);
 		const locationTemperature = await getTemperatureForGeocodedLocation(
 			geocodedLocation
 		);
 		return new Temperature({
-			location: locationName,
+			location,
 			celcius: locationTemperature.temperature,
 		});
 	}
